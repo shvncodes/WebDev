5a9a8a8aae3c647d5c102da6d3fb3819b333855c
Shivani Choudhary <shivanichoudhary2206@gmail.com>
2025-08-31 00:05:49 +0530

    Java Script

---
+++ javaScript-code/basicsJs/script.js
@@ -0,0 +1,5 @@
+let first = document.getElementById("firstPara");
+let mainHeading = document.getElementsByClassName("heading");
+first.style.background="pink";
+mainHeading[0].style.color="red";
+console.log(mainHeading);

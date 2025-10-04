0270bce0b0407dc81c0dcd1a6a7419ed1366dde0
Shivani Choudhary <shivanichoudhary2206@gmail.com>
2025-10-04 20:59:08 +0530

    Classes & Objects

---
+++ javaScript-code/basicsJs/classes.js
@@ -0,0 +1,172 @@
+// A class is like a blueprint or template for creating objects.
+    // Template-
+        // class MyClass {
+        //     constructor() {...}
+        //     myMethod() {...}
+        // }
+//  to create an object with the help of class- 
+    // let myObj = new MyClass();    //new MyClass() use bcz -> JavaScript creates a new empty object.
+
+
+// Constructor() method is special method:
+    // - automatically invoked(Call it to make it run) by new ((let obj = new MyClass();) -> new constructor ko dhundhta hai aur agr ye nhi mila to js apne aap hi constructor ko create karta hai)
+    // -  use to initializes the object
+
+
+class ToyotaCar {
+    constructor(color, mileage) {
+        console.log("creating new object");
+        // initilize the variables & values inside the constructor
+        this.carColor = color;
+        this.mileage = mileage;
+    }
+    start() {
+        console.log("start")
+    }
+    setBrand(brand) {
+        //this means each individual object jiske liye bhi setBrand call hoga
+        this.brandName = brand;
+    }
+    stop() {
+        console.log("stop");
+    }
+}
+
+let fortuner = new ToyotaCar("red", 10); //constructor invoke (creating new object)
+// Uses the class’s method to adding a property named "brandName" to the fortuner object.
+fortuner.setBrand("Fortuner");
+// directly adding a property named "name" to the fortuner object.
+fortuner.name = "Shivani";
+
+let lexus = new ToyotaCar("blue", 12); //constructor invoke (creating new object)
+lexus.brandName = "Lexus";
+lexus.carNumber = 1234;
+
+
+// Inheritance: It is passing down properties & methods from parent class to child(or derived) class.
+// extends use to inheritate
+// if child & parent have same method, child's method will be used. [Method overriding]
+
+// super keyword: it is used to call the constructor of its parent class to access the parent's property & methods.
+// super() -> call's Parent's constructor
+// super.parentMethod() -> call's Parent's method
+
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+    eat() {
+        console.log("eat");
+    }
+    type() {
+        console.log("early morning");
+    }
+    sleep() {
+        console.log("sleep");
+    }
+}
+
+class Engineer extends Person{
+    constructor(name) {
+        super(name);    //to invoke Parent's class constructor
+        console.log("Hello!");
+    }
+    type() {
+        console.log("late night");
+    }
+    work() {
+        super.eat();
+        console.log("solve problem, build something");
+    }
+}
+
+let engObj = new Engineer("Shivani");
+
+
+//--------------------------------------------------------------------
+
+// Example:
+class User {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    viewData() {
+        console.log(this.name, "you are enrolled");
+    }
+}
+
+class Admin extends User {
+    constructor(name, email) {
+        super(name, email);
+    }
+
+    editData(newName, newEmail) {
+        console.log(this.name = newName, this.email = email, "Successfully edit!!");
+    }
+}
+
+let student = new Admin("Shivani", "shivani@gmail.com");
+student.editData("Nisha", "nisha@gmail.com");
+
+//----------------------------------------------------------------------------------------------
+
+// Real life example of classes & objects.
+// Object -> Actual entity  => Blueprint -> of a real entity  => Inheritance
+
+// Object -> Actual entity
+const mobilePhone = {
+    id: "digjr034h8grwobhgw0",
+    type: "SMART_PHONE",
+    size: "6.7",
+    brand: "Nothing",
+    modelNo: "Cmf Phone 2 Pro",
+    ram: "8GB",
+    rom: "128GB",
+    color: "orange",
+    call: () => {console.log("calling....")},
+    gallery: () => {console.log("Show images")},
+    clock: () => {console.log("Time: 10:30 AM")},
+    // ....etc
+}
+
+// Blueprint -> of a real entity
+class MobilePhone {
+    constructor(id,type,size,brand,modelNo, ram, rom, color) {
+        this.id = id;
+        this.type = type;
+        this.size = size;
+        this.brand = brand;
+        this.modelNo = modelNo;
+        this.ram = ram;
+        this.rom = rom;
+        this.color = color;
+        //(not methods)
+        this.speaker = 10; //constant for all phones
+    }
+
+    call() {
+        if(this.type === "SMART_PHONE") {
+            console.log("5G Calling... from ", this.brand);
+        } else {
+            console.log("calling... from ", this.brand)
+        }
+    }
+
+    //all others methods like gallery(), clock(), .....etc
+}
+
+const realmeMobile = new MobilePhone("43323242", "SMART_PHONE", "6.67", "Realme", "realme 7", "4GB", "128GB", "Blue");
+
+// Inheritance
+class SmartPhone extends MobilePhone {
+    constructor(id,type,size,brand,camera,...) {
+        super(id, type,...);
+        this.camera = camera;
+    }
+
+    clickPicture() {
+        //....
+    }
+}
--- javaScript-code/basicsJs/objects.js
+++ javaScript-code/basicsJs/objects.js
@@ -1,15 +1,33 @@
-
+// variable define -
 const age = 19;
 const name = "Shivani"
 const weight = 30.5
-const arr = [1,2,3,4,5]
+const arr = [1, 2, 3, 4, 5]
 
+            // Object can contain - (property & behavior) variable, function, array, string, object, etc.
+
+//Example 1
+const student = {
+    //property -
+    name: "Shivani",
+    marks: 72,
+    //behavior -
+    printMarks: function () {
+        console.log("marks =", this.marks);  //this.marks => student.marks
+    }
+}
+
+//Example 2
 const mobilePhone = {
     camera: 4,
     size: 6.7,
     ram: "8 GB",
     isSmartPhone: true,
-    call: () => {
+    // call: function() {
+    //     console.log("calling....");
+    // },
+                //OR,
+    call() {
         console.log("calling....");
     },
     apps: [
@@ -24,14 +42,11 @@ const mobilePhone = {
             console.log(".... Low volume....", v)
         }
     }
-} 
-
-
-
+}
+//how to access any object element 
 mobilePhone.setting.lowVolume(80);
 
-
-
+//Example 3 (object for each product in amazon)
 const data = {
     Publisher: 'Wisdom Tree; First Edition (13 May 2023); Wisdom Tree, 4779/23, Ansari Road, New Delhi-2. www.wisdomtreeindia.com, wisdomtreebooks@gmail.com',
     rating: {
@@ -43,7 +58,58 @@ const data = {
     }
 }
 
+//Example 4 (object of a single page)
+const page = {
+    padeID: "id",
+    content: "text",
+    name: "name",
+    location: "add",
+    createdAt: "date",
+    updatedAt: "update",
+    isDeleted: false
+}
 
+//Example 5 (chatgpt object for each chat)
+const chat = {
+    id: '68cebac9-5700-8324-a617-38fe2469e668',
+    name: 'Import in vanilla JS',
+    content: 'grbgregi4orhyfo834fg34g 5gh54g9854g5450',
+    createdAt: '20 Sep 2025, 7:00PM',
+    createdBy: 'shivani-account-id'
+}
+
+            // Prototype: Objects have a special property called prototype(reference to an object).
+
+const employee = {
+    calcTax() {
+        console.log("the tax is 10%");
+    }
+}
+
+const arjun = {
+    salary: 5000,
+}
+
+const karan = {
+    salary: 20000,
+    calcTax() {
+        console.log("the tax is 20%");
+    }
+}
+arjun.calcTax() //result: arjun.calcTax is not a function.
+
+// we can set prototype using object1. _ _ proto _ _ = "object2"
+arjun.__proto__ = employee;
+karan.__proto__ = employee;
+
+arjun.calcTax(); // result: the tax is 10%
+
+//if object & prototype have same method, object's method will be used.
+karan.calcTax(); // result: the tax is 20%
+
+
+
+//-----------------------------------------------------------------------------------------------------------------
 
 // Google:- Search, Gmail, GooglePay, Youtube, Android, Google Cloud AI,...
 // Meta:- Facebook, Instagram, WhatsApp.... MetaAI
@@ -59,24 +125,8 @@ const data = {
 // CTO - Shivani (Vedha)
 
 
-const page = {
-    padeID: "id",
-    content: "text",
-    name: "name",
-    location: "add",
-    createdAt: "date",
-    updatedAt: "update",
-    isDeleted: false
-}
 
-// chatgpt object for each chat
-const chat = {
-   id: '68cebac9-5700-8324-a617-38fe2469e668',
-   name: 'Import in vanilla JS',
-   content: 'grbgregi4orhyfo834fg34g 5gh54g9854g5450',
-   createdAt: '20 Sep 2025, 7:00PM',
-   createdBy: 'shivani-account-id'
-}
+//For import an object file in your js file
 
 export const questions = [
     {
@@ -93,5 +143,3 @@ export const questions = [
     }
 ]
 // import {questions} from "./objects"
-
-
